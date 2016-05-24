d18eeb7961c6b6e146e57a7d0a951e11e0409582
Báder László <bader.laszlo22@gmail.com>
2016-05-24 16:31:05 +0200

    End of day commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-    
+    $('.process .navigation a').on('click', function  (e) {
+
+    	e.preventDefault();
+
+    	$('.process .navigation a').removeClass('active');
+
+    	$(this).addClass('active');
+    	   
+    });
+
 });
