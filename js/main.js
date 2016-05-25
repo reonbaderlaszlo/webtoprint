39c938b01f3cd5b4475eab17d47d3bdff5bf4966
Báder László <bader.laszlo22@gmail.com>
2016-05-25 10:28:04 +0200

    First commit today, dont worry its good

--- js/main.js
+++ js/main.js
@@ -1,12 +1,67 @@
-$(function(){
-    $('.process .navigation a').on('click', function  (e) {
+$(function() {
+    $('.process .navigation a').on('click', function(e) {
 
-    	e.preventDefault();
+        e.preventDefault();
 
-    	$('.process .navigation a').removeClass('active');
+        $('.process .navigation a').removeClass('active');
 
-    	$(this).addClass('active');
-    	   
+        $(this).addClass('active');
+
+    });
+
+    $('.hero-slider .slider li').first().css('display','block');
+
+    $('.big-next').on('click', function(e) {
+
+        e.preventDefault();
+
+    	if ($('.hero-slider .slider li.active').next().length) {
+
+    		$('.hero-slider .slider li').fadeOut(500);
+
+            $('.hero-slider .slider').find('li.active').removeClass('active').next().addClass('active').fadeIn(500);
+
+
+        } else {
+
+        	$('.hero-slider .slider li').removeClass('active').fadeOut(500);
+
+            $('.hero-slider .slider li').first().addClass('active').fadeIn(500);
+
+        }
+
+    	if($('.hero-slider .slider .s-1').hasClass('active')){
+
+    		$('#index .hero-slider').css({
+
+    			'background-color': '#f57c00'
+
+    		});
+
+    		$('#index .hero-slider .big-next').css({
+
+    			'background-color': '#5c6bc0'
+
+    		});
+
+    	}
+
+    	if($('.hero-slider .slider .s-2').hasClass('active')){
+
+    		$('#index .hero-slider').css({
+
+    			'background-color': '#5c6bc0'
+
+    		});
+
+    		$('#index .hero-slider .big-next').css({
+
+    			'background-color': '#f57c00'
+
+    		});
+
+    	}     
+
     });
 
 });
